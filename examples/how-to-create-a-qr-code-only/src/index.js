0c44955d04e79af13c7c175dcb8e88dbaaf3bafb
Felix Graule <felix.graule@evorest.ch>
2023-11-15 15:45:43 +0100

    Add example to generate SVG with code only

---
+++ examples/how-to-create-a-qr-code-only/src/index.js
@@ -0,0 +1,29 @@
+import { writeFileSync } from "fs";
+import { SwissQRBill } from "swissqrbill/svg";
+
+
+const data = {
+  amount: 1994.75,
+  creditor: {
+    account: "CH44 3199 9123 0008 8901 2",
+    address: "Musterstrasse",
+    buildingNumber: 7,
+    city: "Musterstadt",
+    country: "CH",
+    name: "SwissQRBill",
+    zip: 1234
+  },
+  currency: "CHF",
+  debtor: {
+    address: "Musterstrasse",
+    buildingNumber: 1,
+    city: "Musterstadt",
+    country: "CH",
+    name: "Peter Muster",
+    zip: 1234
+  },
+  reference: "21 00000 00003 13947 14300 09017"
+};
+
+const svg = new SwissQRBill(data);
+writeFileSync("qr-code-only.svg", svg.toString());
